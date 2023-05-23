5b89fb099bc11ea754893df1bbf515e11c22e0cb
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-05-23 15:56:09 +0900

    loaded first css stylesheet as a static using load static

--- food/views.py
+++ food/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
     items_list = Item.objects.all()
-    return HttpResponse(items_list)
+    # template = loader.get_template('food/index.html')
+    #the context passed into the template is the data from the database
+    context = {
+        'items_list':items_list,
+    }
+    # return HttpResponse(template.render(context,request))
+    return render(request,'food/index.html', context)
 
 def item(request):
     return HttpResponse("This is one of the food items on our menu")
+
+def detail(request, item_id):
+    menu_item = Item.objects.get(pk=item_id)
+    context = {
+        "item":menu_item,
+    }
+    return render(request, 'food/detail.html', context)
+    # return HttpResponse(template.render(context, request))
