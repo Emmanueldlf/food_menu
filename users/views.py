8ba65db2c06a5cbb3e76af6ae1ef65fc30b16294
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-05-25 22:58:39 +0900

    created login page and redirected logged in user to the index page

--- users/views.py
+++ users/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import redirect,render
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from .forms import RegisterForm
 
 # Create your views here.
 
 def register(request):
     if request.method == "POST":
-        user_form = UserCreationForm(request.POST)
+        user_form = RegisterForm(request.POST)
         if user_form.is_valid():
             user_form.save()
             username = user_form.cleaned_data.get('username')
             messages.success(request,f'Welcome {username}, your account was successfully created!')
             return redirect('food:index')
     else:
-        user_form = UserCreationForm()
+        user_form = RegisterForm()
     return render(request,'users/register.html', {'form':user_form})
