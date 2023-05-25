cf243dba7d758f5c72f3a6cfaf3a57c9584d9dff
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-05-26 00:08:10 +0900

    image is now displayed when stored locally and loaded as a static

--- food_menu/urls.py
+++ food_menu/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include,path
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,7 @@ urlpatterns = [
     path('profile/',user_views.profile,name="profile"),
 
 ]
+
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
