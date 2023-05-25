77a1677569e49d974a4dbeee4b5527707152cfc6
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-05-25 18:42:52 +0900

    implemented the user registration feature with a registration form and success message

---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import redirect,render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            username = user_form.cleaned_data.get('username')
+            messages.success(request,f'Welcome {username}, your account was successfully created')
+            return redirect('food:index')
+    else:
+        user_form = UserCreationForm()
+    return render(request,'users/register.html', {'form':user_form})
