90502ac3928425d0c46328d600ec3b53f3f54df2
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-05-25 23:35:11 +0900

    redirected to the login page for users who are not login when they click on profile

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect,render
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import RegisterForm
 
@@ -15,3 +16,7 @@ def register(request):
     else:
         user_form = RegisterForm()
     return render(request,'users/register.html', {'form':user_form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
